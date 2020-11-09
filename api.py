25eddd9f488909a796d93107903b848369cb265e
jaredsparta <73166572+jaredsparta@users.noreply.github.com>
2020-11-09 14:07:41 +0000

    if statement

--- api.py
+++ api.py
@@ -6,3 +6,5 @@ response = requests.get("https://www.bbc.co.uk/")
 print(emojize(":thumbs_up:"))
 print(response)
 
+if response.status_code == 200:
+    print("Up and running")
--- modules.py
+++ modules.py
@@ -15,16 +15,14 @@ def round_task():
 ## os module 
 import os, datetime, sys
 
-working_dir = os.getcwd()
-print(working_dir)
-
 # # Does not work in windows
 # print(os.uname())
 
-print(os.cpu_count()) # Prints number of CPU cores
+# print(os.cpu_count()) # Prints number of CPU cores
 
-print(datetime.datetime.today()) # Returns current system datetime
+# print(datetime.datetime.today()) # Returns current system datetime
 
-print(sys.path) # Allows one to see system paths
+#print(sys.path) # Allows one to see system paths
 
-print()
+import requests
+response
