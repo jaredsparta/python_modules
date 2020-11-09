ad21d911493a72c7b7120f9c4dd9f1d768f1180d
jared <jtlss14@gmail.com>
2020-11-09 16:17:52 +0000

    pseudo code added

--- postcode_task.py
+++ postcode_task.py
@@ -14,17 +14,30 @@ import json
 # print(str(live_response.content))
 
 def get_ll():
+    # Asks user to input a postcode
     arg = input("Please enter your postcode: ")
     postcode = "http://api.postcodes.io/postcodes/" + arg
+
+    # This gets the status code from the site
+    # If not valid, the function ends
     response = requests.get(postcode)
     print(response.status_code)
     if not response.status_code == 200:
         return print("Sorry, not in database")
+   
+    # This receives the content from the API
     val_1 = response.content
+    # I use json.loads() to turn the bytes into a dictionary
     dic = json.loads(val_1)
+
+    # As required, I print out the latitude and longitude of the postcode
     print(f"latitude: {dic['result']['latitude']}")
     print(f"longitude: {dic['result']['longitude']}")
+
+    # I put this in so people can choose when to see the rest of the results
     input("\nPress enter to see results")
+    
+    # This prints out the rest of the results that the API returns
     for key, value in dic['result'].items():
         print(f"{key} : {value}")
 
